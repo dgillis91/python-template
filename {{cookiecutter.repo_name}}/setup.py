4142d84bbc732b9b9490276043ff8281199fe7fb
David Gillis <dgillis91@gmail.com>
2023-09-04 14:32:49 -0500

    init

---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="{{cookiecutter.repo_name}}",
+    version="{{cookiecutter.version}}",
+    description="{{cookiecutter.description}}",
+    author="{{cookiecutter.author}}",
+    author_email="{{cookiecutter.email}}",
+    url="{{cookiecutter.github_url}}",
+)
